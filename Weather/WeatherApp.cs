e1f93805036de1ce1b796b14285f0135c842f663
subha2803 <subha.g2803@gmail.com>
2015-04-17 18:43:23 -0500

    Added a new project Weather

---
+++ Weather/WeatherApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.Net;
+
+namespace Weather
+{
+    class WeatherApp
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("enter your zipcode");
+            var zipcode = Console.ReadLine();
+            var url = String.Format("http://api.openweathermap.org/data/2.5/forecast/daily?q={0}&mode={1}&units=metric&cnt=2",zipcode,"xml");
+            var Client = new HttpClient();
+            var response = Client.GetAsync(url).Result;
+            var responseContent = response.Content;
+            string responsestring = responseContent.ReadAsStringAsync().Result;
+            Console.WriteLine(responsestring);
+
+        }
+    }
+}
